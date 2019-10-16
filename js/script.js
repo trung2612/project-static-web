7e36334efcd7abaf3bbd1d942cb9ec7fdc7435c5
trung2612 <nguyenhatrung.nguyenha@gmail.com>
2019-10-16 15:14:18 +0700

    lam web

--- js/script.js
+++ js/script.js
@@ -13,10 +13,10 @@ $.getJSON('js/data.json', function(data) {
 	fruit.forEach(function (element) {
 		fruitdatabase += ` 
 		<div class="list__sp col-12 col-md-3">
-            <div>
-               <img class="img-fluid"  style="object-fit: cover;height: 175px;" src="images/${element.img}" alt="anh">
-            </div>
-            <p class="list__sp--title">${element.name}</p>
+            <a href="product.html"><div>
+               <img class="img-fluid"   src="images/${element.img}" alt="anh">
+            </div></a>
+            <a href="product.html"><p class="list__sp--title">${element.name}</p></a>
             <p class="list__sp--price">${element.price}&#160VND</p>
           </div>
 		`
@@ -55,4 +55,18 @@ $.getJSON('js/data.json', function(data) {
 	$('.juce__list').html(fruitdatabase);
 
 	
+});
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 600);
 });
